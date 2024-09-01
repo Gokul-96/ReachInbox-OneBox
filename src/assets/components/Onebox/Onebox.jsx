e431c18a053d291c8f57e3aa2aa9628208d326b5
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-01 07:16:46 +0530

    step6

--- src/assets/components/Onebox/Onebox.jsx
+++ src/assets/components/Onebox/Onebox.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ReplyForm from './ReplyForm';
-import './onebox.css';
+import './Onebox.css';
 
 const Onebox = () => {
   const [threads, setThreads] = useState([]);
